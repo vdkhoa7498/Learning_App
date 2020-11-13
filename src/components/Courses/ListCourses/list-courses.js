5541630b5496ce8cf9f1444877c8eea44b8f3fbd
vdkhoa7498 <vdkhoa7498@gmail.com>
2020-11-13 23:39:28 +0700

    Update list-courses search

--- src/components/Courses/ListCourses/list-courses.js
+++ src/components/Courses/ListCourses/list-courses.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { StyleSheet, View, FlatList } from 'react-native';
-import ListCoursesItem from '../ListCoursesItem/list-courses-item'
+import { StyleSheet, View, FlatList, TextInput, Button } from 'react-native';
+import ListCoursesItem from '../ListCoursesItem/list-courses-item';
+
 export default function ListCourses(props) {
   const courses = [
       {
@@ -18,13 +19,38 @@ export default function ListCourses(props) {
         level: "Advance",
         released: "Nov 6, 2020",
         duration: "30 hours"
-    }
+    },
+    {
+      id: 3,
+      title: "React Native",
+      author: "Hai Pham",
+      level: "Advance",
+      released: "Nov 6, 2020",
+      duration: "30 hours"
+  }
   ]
+
+  const searchView = () => {
+    return (
+      <View style={{flexDirection: 'row'}}>
+        <TextInput 
+          placeholder = 'search text'
+          style = {{flex: 1, borderColor: 'gray', borderWidth: 1}}
+          />
+        <Button 
+          onPress = {() => {console.log('search')}}
+          title = 'Search'
+          style = {{width: 60, height: 40, margin: 5}}
+        />
+      </View>
+    );
+  }
 
   return (
     <View style={styles.container}>
       <FlatList data={courses}
         renderItem = {({item})  => <ListCoursesItem item={item}/>}
+        ListHeaderComponent = {() => searchView()}
       />
     </View>
   );
