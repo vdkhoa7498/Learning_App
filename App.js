339b6f8667cc65a493a292f9197be9d974261339
vdkhoa7498 <vdkhoa7498@gmail.com>
2020-11-14 15:10:50 +0700

    Create components left and Navigation

--- App.js
+++ App.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler'
 import Home from './src/components/Main/Home/Home'
 import ListCourses from './src/components/Courses/ListCourses/list-courses'
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <Home/> */}
-      <ListCourses/>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="List Courses" component={ListCourses} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/components/Authentication/VerifyPassword/VerifyPassword.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, Image } from 'react-native';
+
+export default function ForgetPassword() {
+  return (
+    <View style={styles.container}>
+      <Image source = {require('../../../../assets/login.png')}/>
+      <TextInput
+        style = {styles.textInput}
+        placeholder = "User Name"
+      />
+      <TextInput
+        style = {styles.textInput}
+        placeholder = "Verify Code"
+      />
+      <TouchableOpacity 
+        style = {styles.btn}>
+        <Text style = {styles.btnText}>Reset Password</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textInput:{
+    height: 40,
+    marginTop: 20,
+    textAlign: "center",
+    borderColor: "gray",
+    borderWidth: 1
+  },
+  btn:{
+    height: 40,
+    marginTop: 20,
+    width: "98%",
+    backgroundColor: "blue",
+    alignItems: "center"
+  },
+  btnText:{
+    justifyContent: "center",
+    textAlign: "center",
+  }
+});
